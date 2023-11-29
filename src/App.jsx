c6fd565e3c70c3a2074f5d58c9ac17f8af5b2273
VPontis15 <94546957+VPontis15@users.noreply.github.com>
2023-11-29 23:53:54 +0200

    finish the country page functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import Layout from "./features/layout/Layout";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./features/pages/Homepage";
-import CountryDetails from "./features/pages/CountryDetails";
+
+import Country from "./features/pages/Country";
+import { useState } from "react";
 
 function App() {
   const queryClient = new QueryClient();
+  const [queryOption, setQueryOption] = useState("all");
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -15,9 +18,33 @@ function App() {
         <Layout>
           <BrowserRouter>
             <Routes>
-              <Route index element={<Homepage />} />
-              <Route path=":region" element={<Homepage />} />
-              <Route path="country/:name" element={<CountryDetails />} />
+              <Route
+                index
+                element={
+                  <Homepage
+                    queryOption={queryOption}
+                    setQueryOption={setQueryOption}
+                  />
+                }
+              />
+              <Route
+                path=":region"
+                element={
+                  <Homepage
+                    queryOption={queryOption}
+                    setQueryOption={setQueryOption}
+                  />
+                }
+              />
+              <Route
+                path="country/:name"
+                element={
+                  <Country
+                    queryOption={queryOption}
+                    setQueryOption={setQueryOption}
+                  />
+                }
+              />
             </Routes>
           </BrowserRouter>
         </Layout>
