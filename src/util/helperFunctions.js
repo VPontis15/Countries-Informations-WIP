59c21c4e36ea808531484cf9e96ca8ca92284d5b
VPontis15 <94546957+VPontis15@users.noreply.github.com>
2023-12-03 18:39:42 +0200

    add helperFunction file with formattNumber function

---
+++ src/util/helperFunctions.js
@@ -0,0 +1,21 @@
+export function numberFormatter(number) {
+  const numStr = number.toString();
+  const numArray = numStr.split("");
+  const len = numArray.length;
+
+  let formattedNum = "";
+  let count = 0;
+
+  for (let i = len - 1; i >= 0; i--) {
+    count++;
+    formattedNum = numArray[i] + formattedNum;
+
+    if (count === 3 && i) {
+      formattedNum = "." + formattedNum;
+      count = 0;
+    }
+  }
+
+  return formattedNum;
+}
+console.log(numberFormatter(988002));
